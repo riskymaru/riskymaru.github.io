7946b16da699392f829d6dedeabc98b138a76706
riskymaru <biz_ricky@yahoo.com>
2021-06-10 22:43:16 +0800

    regv3

--- sw.js
+++ sw.js
@@ -4,6 +4,16 @@ self.addEventListener('install', (e) => {
 		'index.html',
 		'assets/css/main.css',
 		'assets/proj/sfx.js',
+		'assets/sndm4a.bgm1.m4a',
+		'assets/sndm4a/sfx-btn2.m4a',
+		'assets/sndm4a/sfx-btn1.m4a',
+		'assets/sndm4a/sfx-win.m4a',
+		'assets/sndm4a/sfx-wrong.m4a',
+		'assets/sndm4a/play-score.m4a',
+		'assets/sndm4a/ton-click.m4a',
+		'assets/sndm4a/"sfx-tick.m4a',
+		'assets/sndm4a/"sfx-beep.m4a'
+		["sfx-reveal-score",false,this.SFX_VOLUME]
 	  ])),
 	);
 	console.log("reg")
@@ -17,4 +27,4 @@ self.addEventListener('install', (e) => {
   });
   
 
-  console.log("REG v0.2")
+  console.log("REG v0.3")

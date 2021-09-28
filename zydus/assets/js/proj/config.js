04096895a5ee6231f8f5da5ce28b81a5389df9d7
riskymaru <biz_ricky@yahoo.com>
2021-09-28 22:14:00 +0800

    - added zydus

---
+++ zydus/assets/js/proj/config.js
@@ -0,0 +1,23 @@
+"use strict";
+
+class Config{
+
+	constructor(){
+
+        this.gameDimension;
+
+        this.pixiSettings = {
+        						legacy:'true',
+							    width: 1280, 
+							    height: 720, 
+							    backgroundColor: 0x0, 
+							    //resolution: 0.58
+							}
+
+
+
+    }
+}
+
+
+console.log("zzzz")
